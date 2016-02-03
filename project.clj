c59aa79aa96b880e8af06ca1e3577b9b455b5470
Marcos <marcoslamuria@gmail.com>
2016-02-03 10:49:07 +0000

    use dependencies library

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-time "0.11.0"]])
+
+
+  :dependencies.edn "https://raw.githubusercontent.com/clanhr/dependencies/master/dependencies.edn"
+
+  :dependency-sets [:clojure :common]
+  :dependencies []
+
+  :plugins [[clanhr/shared-deps "0.2.6"]])
